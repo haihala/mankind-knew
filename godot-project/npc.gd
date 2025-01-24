f98ed0782d23a49b286aa2b6a56bd78079e61e24
Eero Häihälä <eero.haihala@gmail.com>
2025-01-24 21:32:34 +0200

    Add NPC that wonders around

---
+++ godot-project/npc.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var speed: float = 50
+var next_direction: Vector2
+var prev_direction: Vector2
+
+func _physics_process(delta: float) -> void:
+	act_on_decisions()
+
+func update_decisions():
+	prev_direction = next_direction
+	next_direction = Vector2.from_angle(randf()*PI*2)
+
+func shoot():
+	print("shoot")
+
+func act_on_decisions():
+	var phase = $DecisionTimer.time_left / $DecisionTimer.wait_time
+	var direction = phase * prev_direction + (1-phase) * next_direction
+	velocity = direction * speed
+	move_and_slide()
--- godot-project/player.gd
+++ godot-project/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: float = 200
+@export var speed: float = 150
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
