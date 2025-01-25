a5b5b9eef7d386bf5102d1830e02db7e0bcc3966
Eero Häihälä <eero.haihala@gmail.com>
2025-01-25 16:46:48 +0200

    Scale and reset speech bubble duration on hit

--- godot-project/projectile.gd
+++ godot-project/projectile.gd
@@ -6,12 +6,11 @@ static var size = 0.2
 @export var speed: float = 50
 @export var sprites: Array[Texture2D] = []
 @export var sounds: Array[AudioStream] = []
+@export_range(0, 4) var on_hit_grow: float = 1
 @export var sparkle_scene: PackedScene
 var belief: NPC.Belief
 var creator: Node
 var touched: Array[Node] = []
-
-# Ammu ajatuskuplan kasvu yli
 
 func _ready() -> void:
 	$Sprite.texture = sprites[belief]
@@ -24,9 +23,15 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var velocity = Vector2.from_angle(rotation) * speed
-	var alpha = ($DespawnTimer.time_left / $DespawnTimer.wait_time)
+	var alpha = $DespawnTimer.time_left / $DespawnTimer.wait_time
 	$Sprite.modulate.a = alpha
 	position += velocity * delta
+	
+	# Scale self on hit
+	var scale_phase = $SizeAdjustmentTimer.time_left / $SizeAdjustmentTimer.wait_time
+	var ease = 1-pow(2*scale_phase-1, 2)
+	var scaler = 1 + ease * on_hit_grow
+	scale = Vector2(scaler, scaler)
 
 func collision(body: Node2D) -> void:
 	if body == creator or body in touched:
@@ -39,3 +44,8 @@ func collision(body: Node2D) -> void:
 		var sparkle = sparkle_scene.instantiate()
 		sparkle.position = position
 		get_parent().add_child(sparkle)
+		
+		$SizeAdjustmentTimer.start()
+		# Restart despawn timer
+		# This increases the time exponentially, growing for each encounter
+		$DespawnTimer.start(1.2*$DespawnTimer.wait_time)
